3c6d52a93908e86f3ff7477cfe61a3a89555aa7e
yosefalan <yosefalan@gmail.com>
2022-10-26 16:25:31 -0500

    deploying to heroku

--- app.js
+++ app.js
@@ -1,68 +1,14 @@
-const getUsers = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  const users = await res.json();
-  return users
-}
+const express = require("express")
+const app = express()
 
-const getPosts = async (id) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-  const posts = await res.json()
-  return posts
-}
 
-const loadData = async () => {
-  let userData = "";
-  let postData = "";
-  let selectedUserId;
-  let selectedUserName;
-  const tableBody = document.getElementById('table_data');
-  const postsContainer = document.getElementById('posts_container');
+app.use(express.static("public"))
 
-  const users = await getUsers();
 
-  for(let user of users) {
-  userData += `<tr id="${user.id}"><td>${user.id}</td><td>${user.name}</td><td>${user.username}</td></tr>`;
-  tableBody.innerHTML = userData;
-  }
+// app.get("/", function (req, res) {
+//   res.send("<h1>Hello World!</h1>")
+// })
 
-  const displayPosts = (posts, name) => {
-    postData = '';
-    postData += `<div class="card mb-3" style="background-color: #563D7C; color: white"><div class="card-title"><h3>Displaying Posts from ${name}</h3></div></div>`
-    posts?.map((post) => {
-      postData += `<div class="card mb-3" ><div class="card-title"><h3>${post.title}</h3></div><div class="card-body">${post.body}</div></div>`
-    })
-    postsContainer.innerHTML = postData;
-  }
 
-  const handleClick = async (e) => {
-    const element = e.target.parentNode;
-    const parent = element.parentNode;
-    selectedUserId = element.id;
-    selectedUserName = ""
-    const posts = await getPosts(selectedUserId);
-    const children = [...parent.children];
-    const siblings =  children.filter(child => child !== element);
-
-    element.classList.add('highlight');
-    for (let sibling of siblings) {
-      sibling.classList.remove('highlight')
-    }
-
-    for (let user of users) {
-      console.log(user.id == selectedUserId)
-      if (user.id == selectedUserId) {
-        selectedUserName = user.name
-      }
-    }
-    displayPosts(posts, selectedUserName);
-  }
-
-  const trs = document.getElementsByTagName("tr");
-
-  for (let tr of trs) {
-    tr.addEventListener("click", handleClick);
-  }
-
-}
-
-loadData()
+app.listen(process.env.PORT || 3000,
+	() => console.log("Server is running..."));
